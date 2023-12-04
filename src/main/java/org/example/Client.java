ce8761d9986bd606d9476605af0d690b82ab3744
Timofey Zab <tim18125@gmail.com>
2023-12-04 21:12:17 +0300

    INIT COMMIT

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String host = "localhost";
+        int port = 8080;
+         Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out.println("TIMOFEY ZABOLOTKO");
+            String resp = in.readLine();
+            System.out.println(resp);
+
+
+    }
+}
+
